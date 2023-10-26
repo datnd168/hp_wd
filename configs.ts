90ec3a5203248389bd2e499e75fe260fee4f53ac
danionguyen <datnd168@gmail.com>
2023-10-26 13:54:05 +0700

    update address

--- configs.ts
+++ configs.ts
@@ -7,8 +7,8 @@ const configs = {
   wifeBankName: "Nguyen Thi Nhat",
   address_husband: "Xóm 1, Thôn Yên Mỹ, Xã Bình Yên, Huyện Thạch Thất, Hà Nội",
   address_wife: "Xóm 3, Thôn Yên Mỹ, Xã Bình Yên, Huyện Thạch Thất, Hà Nội",
-  time_wedding: "2023-10-28T12:00:00",
-  txtTimeWedding: "Chủ nhật ngày 28 tháng 10 năm 2023",
+  time_wedding: "2023-10-29T12:00:00",
+  txtTimeWedding: "Chủ nhật ngày 29 tháng 10 năm 2023",
 };
 
 export default configs;
